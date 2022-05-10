0fd18752c4b2dd529d5f1c9983160410e2ac7b20
khekrn <kishorenayark@gmail.com>
2022-05-10 09:23:49 +0530

    Adding sum method

--- main.go
+++ main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello Go !!")
+	fmt.Println("Sum of Array = ", sum(1, 2, 3, 4, 5))
+}
+
+func sum(arr ...int) int {
+	res := 0
+	for _, val := range arr {
+		res += val
+	}
+	return res
 }
